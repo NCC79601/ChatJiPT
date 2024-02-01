5f1174dddb9952cf62ac02fa5d52e00a13380361
郑皓文 <zhenghw21@mails.tsinghua.edu.cn>
2024-02-01 08:21:52 +0800

    清理文件

--- test/test.py
+++
@@ -1,4 +0,0 @@
-from transformers import AutoTokenizer, AutoModel
-tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm3-6b-32k", trust_remote_code=True)
-model = AutoModel.from_pretrained("THUDM/chatglm3-6b-32k", trust_remote_code=True).quantize(4).cuda()
-model = model.eval()
